92e0be773399407a826686fd31496ff8c898839d
Chibuike-Anukam <chibuikeanukam@gmail.com>
2026-06-02 23:18:15 -0400

    Ran stitcher code and successfully stitched image together. Made new script for making the graph Made "System Overview" text file for to do's

--- rectify_and_stitch.py
+++ rectify_and_stitch.py
@@ -29,13 +29,12 @@ OUTPUT_H = 180   # height of final stitched image in pixels --- CHANGE BASED ON 
 # Where each marker should land in the output (top-down view)
 # Adjust these to match the real layout of your markers on the surface
 pts_dst = np.float32([
-    [700,  600],   # marker 1 - 16.5 right, 3cm down     ### CHANGE LOCATION OF ONE POINT AT A TIME AND SEE RESULT
-    [400,  1100],   # marker 2 - 11.25cm right, 9.25cm down   
-    [1000,  1100],   # marker 3 - 16.5cm right, 9.25cm down
-    [700,  1500],   # marker 4 - 21.75cm right, 9.25cm down
-    [700,  1500],   # marker 5 - 16.5cm right, 15.5cm down
+    [165,  30],   # marker 1 - 16.5 right, 3cm down     ### CHANGE LOCATION OF ONE POINT AT A TIME AND SEE RESULT
+    [112.5,  92.5],   # marker 2 - 11.25cm right, 9.25cm down   
+    [165,  92.5],   # marker 3 - 16.5cm right, 9.25cm down
+    [217.5,  92.5],   # marker 4 - 21.75cm right, 9.25cm down
+    [165,  155],   # marker 5 - 16.5cm right, 15.5cm down
 ])
-#### CHANGE POINTS IN BRACKETS TO MATCH THOSE LISTED ON RIGHT
 
 
 # ── 4. COMPUTE HOMOGRAPHIES ────────────────────────────────────────
