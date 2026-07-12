a8182e7de5f0f0d0f0bab227e656c5ca1536f3ae
davidanukam <davidanukam72@gmail.com>
2026-07-12 03:57:32 -0400

    Reworked cm to px formula to use image scaling - Removed web folder, data folder and refImages folder - Refactored complete_parking_... code - TODO: Refactor graph_maker's node_coordinates to start with TL

--- complete_parking_spot_detection_system.py
+++ complete_parking_spot_detection_system.py
@@ -1,20 +1,23 @@
-## This script loads the stitched image and detect the vacant and occupied parking spots ##
+"""This script loads the stitched image and detect the vacant and occupied parking spots"""
+
+# -- Part 1: Predict Parking Spots -- #
+
 from pathlib import Path
 current_dir = Path(__file__).resolve().parent
 
-"""
-##### PART 1: Predict Parking Spots
-# import YOLO
 from ultralytics import YOLO
 
+predict_folder = current_dir / "runs/detect/predict"
 
-# Load trained model
-model = YOLO(current_dir / "runs/detect/train-10/weights/best.pt")
+if not predict_folder.is_dir():
+    # Load trained model
+    model = YOLO(current_dir / "runs/detect/train-10/weights/best.pt")
 
-# Run inference with the YOLO11n model on stitched image of parking lot
-results = model.predict(current_dir / "moc_parking_lot_datatset/test/images/20260622_123826.jpg", save=True, save_txt=True)"""
+    # Run inference with the YOLO11n model on stitched image of parking lot
+    results = model.predict(current_dir / "moc_parking_lot_datatset/test/images/20260622_123826.jpg", save=True, save_txt=True)
 
-##### Part 2: Find centre of vacant spots
+# -- Part 2: Find centre of all parking spots -- #
+
 from rectify_and_stitch import*
 
 
@@ -42,7 +45,7 @@ def get_box_centers(yolo_file_path, image_width, image_height):
 
             # 3. Denormalize to get the exact pixel locations
             pixel_x_center = int(x_center_norm * image_width)
-            pixel_y_center = OUTPUT_H - int(y_center_norm * image_height)
+            pixel_y_center = int(y_center_norm * image_height)
 
             # Store the result (keeping track of the class can be helpful)
             centers.append({
@@ -52,36 +55,37 @@ def get_box_centers(yolo_file_path, image_width, image_height):
 
     return centers
 
-# set image size to size of stitched image
-IMG_W: int = OUTPUT_W
-IMG_H: int = OUTPUT_H
+# -- Part 3: Convert World Coords to Pixel Coords -- #
 
-# file_path = 'C:/Projects/SMART_PARK/runs/detect/predict-3/labels/20260622_123826.txt'
-file_path = current_dir / "runs/detect/predict-3/labels/20260622_123826.txt"
+import cv2
 
-detected_centers = get_box_centers(file_path, IMG_W, IMG_H)
+img = cv2.imread(str(predict_folder / "20260622_123826.jpg"))
+img_height, img_width = img.shape[:2]
+# print(img_width, img_height)
 
-# Note: Print all detected centers and their class status
+file_path = current_dir / predict_folder / "labels/20260622_123826.txt"
+detected_centers = get_box_centers(file_path, img_width, img_height)
+
+# Print all detected centers and their class status
 # for obj in detected_centers:
 #     print(f"Object Class {obj['class_id']} Center Pixel Coordinate: {obj['center']}")
 
-#### Part 3: Convert the Location of the Nodes to Pixel Values
 from graph_maker import*
-import math
 
-SCALE: int = 10
-X_PAD: int = 30
-Y_PAD: int = 15
+real_img_width = 33.5
+real_img_height = 18.4
+
+SCALE_X: float = img_width / real_img_width
+SCALE_Y: float = img_height / real_img_height
+
+X_PAD: int = 3
+Y_PAD: int = 1.5
 
 def node_to_px(node_dict, node_name):
     x_cm, y_cm = node_dict[node_name]
-    return (int(x_cm * SCALE + X_PAD), int(y_cm * SCALE + Y_PAD))
+    return (int((x_cm + X_PAD) * SCALE_X), int((y_cm + real_img_height - Y_PAD) * SCALE_Y))
 
-
-# adj_list = {"a" : [1, 2, 3, 4, 5], "b" : [0, 0, 0]}
-# px_list = {}
-
-# Converting node_coordinates dict to pixel values and saving them to a new dict (node_coordinates_px)
+# Convert node_coordinates dict to pixel values and save them to node_coordinates_px
 node_coordinates_px: dict[list[tuple]] = {}
 
 for key in node_coordinates:
@@ -90,79 +94,52 @@ for key in node_coordinates:
     else:
         node_coordinates_px[key] = node_to_px(node_coordinates, key)
 
-print()
-print("node_coordinates:", node_coordinates)
-print()
-print("node_coordinates_px:", node_coordinates_px)
+print("\nnode_coordinates:", node_coordinates)
+print("\nnode_coordinates_px:", node_coordinates_px)
 
 
-#### Part 4: Find the node in the node coordinate dict corresponding to the one from the image
-cor_nodes: list[str] = []
-accuracy: int = 5
+# -- Part 4: Based on accuracy, get the vacant nodes from node_coordinates -- #
+vacant_nodes: list[str] = []
+accuracy: int = 50
 
-for obj in detected_centers: # detected_centers is a list of dicts, so obj is a dict!
+for obj in detected_centers:
     if obj['class_id'] == 0:
         for node, coord in node_coordinates_px.items():
             if abs(coord[0] - obj['center'][0]) <= accuracy and abs(coord[1] - obj['center'][1]) <= accuracy:
-                cor_nodes.append(node)
+                vacant_nodes.append(node)
 
-print()
-print("cor_nodes:", cor_nodes)
+print("\nvacant_nodes:", vacant_nodes)
 
 
-#### Part 5: Run path finding algorithm
+# -- Part 5: Run path finding algorithm -- #
 
-# --- SIMULATION --- #
-
-# A vacant spot detection algorithm flags these spots as open
-# vacant_spot_set = {'P1', 'P4', 'P9', 'P12'}
-vacant_spot_set: set[str] = set(cor_nodes)
+vacant_spot_set: set[str] = set(vacant_nodes)
 
 best_path, total_meters, chosen_spot = dijkstra(parking_lot_graph, 'TR', vacant_spot_set)
 
-print()
-print(f"Navigation Path: {' -> '.join(best_path)}")
+print(f"\nNavigation Path: {' -> '.join(best_path)}")
 print(f"Total Distance: {total_meters:.2f} centimeters")
 print(f"Chosen Spot: {chosen_spot}")
 
-#### Part 6: Draw nodes and edges on the image
-import cv2
-
-# img = cv2.imread('stitched_topdown.png')
-img = cv2.imread('runs/detect/predict/20260622_123826.jpg')
-
-img_height, img_width = img.shape[:2]
-print(img_width, img_height)
-
-bl_px = (560, 3377)
-def node_to_px(node_dict, node_name):
-    x_cm, y_cm = node_dict[node_name]
-    return (int(bl_px[0] + (x_cm * 500 / 2.51)), int(bl_px[1] - (y_cm * 500 / 2.51)))
-
-node_coordinates_px: dict[list[tuple]] = {}
-
-for key in node_coordinates:
-    if key in node_coordinates_px:
-        node_coordinates_px[key].append(node_to_px(node_coordinates, key))
-    else:
-        node_coordinates_px[key] = node_to_px(node_coordinates, key)
-
-# node_coordinates_px = {'BL' : (560, 3377), 'P5E' : (1669, 3357), 'P6E' : (2712, 3338), 'P6': (2686, 2418)}
-
-print()
-print("node_coordinates_px:", node_coordinates_px)
+# -- Part 6: Draw nodes and edges on the image -- #
 
 for i in range(len(best_path) - 1):
     node1 = node_coordinates_px[best_path[i]]
     node2 = node_coordinates_px[best_path[i + 1]]
-    cv2.line(img, node1, node2, (0, 0, 255), 10) # Color = (Blue, Green, Red) or BGR
+    cv2.line(img, node1, node2, (0, 0, 0), 40) # Color = (Blue, Green, Red) or BGR
 
-for node in best_path:
+for i, node in enumerate(best_path):
     point = node_coordinates_px[node]
-    cv2.circle(img, (point[0], point[1]), 32, (255, 255, 255), -1)
-    cv2.putText(img, node, (point[0] + 32, point[1] + 50), cv2.FONT_HERSHEY_SIMPLEX, 5, (255,255,255), 5)
+    if i == 0:
+        cv2.circle(img, (point[0], point[1]), 48, (0, 255, 0), -1)
+    elif i == len(best_path) - 1:
+        cv2.circle(img, (point[0], point[1]), 48, (0, 0, 255), -1)
+    else:
+        cv2.circle(img, (point[0], point[1]), 48, (0, 255, 255), -1)
+    cv2.putText(img, node, (point[0] + 64, point[1] + 50), cv2.FONT_HERSHEY_SIMPLEX, 5, (255,255,255), 20)
 
-cv2.imwrite('path_overlay.png', img)
+output = str(predict_folder / "path_overlay.png")
+cv2.imwrite(output, img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
